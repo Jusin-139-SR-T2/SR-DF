d1a6420b8eca3033735059ec11c55fb9f7f7e133
Lee Hak Jun <harleybird23@gmail.com>
2023-09-28 17:12:34 +0900

    [학준] 병합전

--- Client/Code/Brown.cpp
+++ Client/Code/Brown.cpp
@@ -22,7 +22,7 @@ HRESULT CBrown::Ready_GameObject()
 
     FAILED_CHECK_RETURN(Add_Component(), E_FAIL);
     
-    m_pTransformComp->Set_Scale({ 200.f, 100.f, 1.f });
+    //m_pTransformComp->Set_Scale({ 200.f, 100.f, 1.f });
     m_pTransformComp->Set_Pos({ 5.f, 1.f, 25.f });
     m_fFrameEnd = 0;
     m_fFrameSpeed = 10.f;
@@ -112,7 +112,7 @@ _int CBrown::Update_GameObject(const _float& fTimeDelta)
 
     // ºôº¸µå --------------------------------------
 
-    FaceTurn(fTimeDelta);
+    //FaceTurn(fTimeDelta);
     
     // --------------------------------------------
 
@@ -133,7 +133,7 @@ _int CBrown::Update_GameObject(const _float& fTimeDelta)
 
     // --------------------------------------------
 
-    Engine::Add_RenderGroup(RNEDER_ALPHATEST, this);
+    Engine::Add_RenderGroup(RENDER_ALPHA, this);
 
     return S_OK;
 }
