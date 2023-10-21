1dfba6fd354849dfeb6d995e8c94f2a78d052636
Lee Hak Jun <harleybird23@gmail.com>
2023-10-21 16:13:23 +0900

    [학준] 플레이어 파싱 데이터에 라디안 변환 넣음

--- Client/Code/Player.cpp
+++ Client/Code/Player.cpp
@@ -148,7 +148,7 @@ HRESULT CPlayer::Ready_GameObject(const FSerialize_GameObject& tObjectSerial)
     FAILED_CHECK_RETURN(Ready_GameObject(), E_FAIL);
 
     m_pTransformComp->Set_Pos(tObjectSerial.vPos);
-    m_pTransformComp->Set_Rotation(tObjectSerial.vRotation);
+    m_pTransformComp->Set_Rotation(D3DXToRadian(tObjectSerial.vRotation));
     //m_pTexture->Set_Scale(tObjectSerial.vScale);
 
     wstring strConvName(tObjectSerial.tHeader.strName.begin(), tObjectSerial.tHeader.strName.end());
